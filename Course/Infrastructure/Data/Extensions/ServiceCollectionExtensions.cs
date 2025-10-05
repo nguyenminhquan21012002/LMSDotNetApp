89e794952f19cdd871e6a81b191e74a80a43af4b
Minh Tran <kin472k2@yahoo.com>
2025-10-05 19:06:44 +0700

    fix violations Clean Architecture

---
+++ Course/Infrastructure/Data/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Course.Core.Domain.Interfaces;
+using Course.Infrastructure.Data.DbContexts;
+using Course.Infrastructure.Data.Mappings;
+using Course.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course.Infrastructure.Data.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Database
+            services.AddDbContext<CourseDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // MongoDB
+            services.AddSingleton<MongoDbContext>();
+
+            // Repositories
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+            // Auto Mapper
+            services.AddAutoMapper(typeof(CourseDocumentMappingProfile));
+
+            return services;
+        }
+    }
+}
